587193ef7f38378bd3d4c6d5a20a5bce3922b3b0
Tef <thominhaserick@gmail.com>
2021-03-16 00:14:40 -0300

    adicionando uri da api

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,29 +1,33 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) {
-  }
+
+  apiURL = environment.apiBaseUrl + 'auth/'
+
+  constructor(private http: HttpClient) { }
+
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(this.apiURL + 'signin', {
       username,
       password
     }, httpOptions);
   }
 
   register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
+    return this.http.post(this.apiURL + 'signup', {
       username,
       email,
       password
--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -1,12 +1,13 @@
-import {Injectable} from '@angular/core';
-
-const TOKEN_KEY = 'auth-token';
-const USER_KEY = 'auth-user';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
+
+  TOKEN_KEY = 'auth-token';
+  USER_KEY = 'auth-user';
+
   constructor() {
   }
 
@@ -15,21 +16,21 @@ export class TokenStorageService {
   }
 
   public saveToken(token: string): void {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token);
+    window.sessionStorage.removeItem(this.TOKEN_KEY);
+    window.sessionStorage.setItem(this.TOKEN_KEY, token);
   }
 
   public getToken(): string | null {
-    return window.sessionStorage.getItem(TOKEN_KEY);
+    return window.sessionStorage.getItem(this.TOKEN_KEY);
   }
 
   public saveUser(user: any): void {
-    window.sessionStorage.removeItem(USER_KEY);
-    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+    window.sessionStorage.removeItem(this.USER_KEY);
+    window.sessionStorage.setItem(this.USER_KEY, JSON.stringify(user));
   }
 
   public getUser(): any {
-    const user = window.sessionStorage.getItem(USER_KEY);
+    const user = window.sessionStorage.getItem(this.USER_KEY);
     if (user) {
       return JSON.parse(user);
     }
