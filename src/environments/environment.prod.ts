7527feea0aacf6ccc0bb2b060cd86f7c8f8fcded
Tef <thominhaserick@gmail.com>
2021-03-25 21:13:43 -0300

    ajuste url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://freedom-api-com-br.umbler.net//api/v1/',
+  apiBaseUrl: 'http://freedom-api-com-br.umbler.net/api/v1/',
   token_header_key: 'x-access-token',
   access_token: 'currentUser'
 };
