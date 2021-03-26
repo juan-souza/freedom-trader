2c967689b52d73a82235bc8bac447299da4ebc85
Tef <thominhaserick@gmail.com>
2021-03-25 21:07:13 -0300

    Atualizando arquivo produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://freedom-api-com-br.umbler.net//api/v1/',
+  token_header_key: 'x-access-token',
+  access_token: 'currentUser'
 };
