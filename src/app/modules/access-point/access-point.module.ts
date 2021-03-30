520c734af51cfa09220d317725b0f7cf70302a9f
Juan Souza <juanccdesouza@gmail.com>
2021-03-29 23:31:10 -0300

    datatable access-point teste

--- src/app/modules/access-point/access-point.module.ts
+++ src/app/modules/access-point/access-point.module.ts
@@ -1,15 +1,43 @@
-import { NgModule } from '@angular/core';
+// Angular
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 
-import { AccessPointRoutingModule } from './access-point-routing.module';
-import { AccessPointComponent } from './access-point.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
+// Tabs Component
+import {AccessPointRoutingModule} from './access-point-routing.module';
+import {AccessPointComponent} from './access-point.component';
+import {DataTablesModule} from 'angular-datatables';
+import {UserService} from '../users/services/user.service';
 
 @NgModule({
-  declarations: [AccessPointComponent],
   imports: [
+    AccessPointRoutingModule,
     CommonModule,
-    AccessPointRoutingModule
-  ]
+    DataTablesModule,
+    FormsModule,
+    RouterModule,
+    ToastrModule.forRoot(
+      {
+        timeOut: 3000,
+        progressBar: false,
+        progressAnimation: "increasing",
+        enableHtml: true,
+        positionClass: 'toast-bottom-right',
+      }
+    ),
+    ModalModule.forRoot()
+
+  ],
+  declarations: [
+    AccessPointComponent
+
+  ], providers: [
+    UserService
+  ],
 })
-export class AccessPointModule { }
+export class AccessPointModule {
+}
